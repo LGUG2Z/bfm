8b62a6b3f05d242d5eb18dec038ecf6521e71534
jiqb <gthbji@ml1.net>
2017-08-22 08:23:57 +0100

    Add a Brewfile representation for packages read in by line

---
+++ brewfile/packages.go
@@ -0,0 +1,65 @@
+package brewfile
+
+import (
+	"io/ioutil"
+	"strings"
+)
+
+type Packages struct {
+	Tap, Brew, Cask, Mas []string
+}
+
+func (p *Packages) FromBrewfile(brewfilePath string) error {
+	bytes, err := ioutil.ReadFile(brewfilePath)
+	if err != nil {
+		return err
+	}
+
+	lines := strings.Split(string(bytes), "\n")
+
+	p.Tap = separate("tap", lines)
+	p.Brew = separate("brew", lines)
+	p.Cask = separate("cask", lines)
+	p.Mas = separate("mas", lines)
+
+	return nil
+}
+
+func (p *Packages) Bytes() []byte {
+	lines := []string{}
+
+	for _, line := range p.Tap {
+		lines = append(lines, line)
+	}
+
+	lines = append(lines, "")
+
+	for _, line := range p.Brew {
+		lines = append(lines, line)
+	}
+
+	lines = append(lines, "")
+
+	for _, line := range p.Cask {
+		lines = append(lines, line)
+	}
+
+	lines = append(lines, "")
+
+	for _, line := range p.Mas {
+		lines = append(lines, line)
+	}
+
+	return []byte(strings.Join(lines, "\n"))
+}
+
+func separate(packageType string, lines []string) []string {
+	var packages []string
+	for _, line := range lines {
+		if strings.HasPrefix(line, packageType) {
+			packages = append(packages, line)
+		}
+	}
+
+	return packages
+}
