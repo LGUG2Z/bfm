f68eae4135e475949e8843f6d3edb0c153c42a31
jiqb <gthbji@ml1.net>
2017-08-23 13:38:58 +0100

    Added BDD layout for remove command tests

--- cmd/remove_test.go
+++ cmd/remove_test.go
@@ -1,19 +1,64 @@
-package cmd
+package cmd_test
 
 import (
-	"reflect"
-	"testing"
+	. "github.com/lgug2z/bfm/cmd"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
-func TestRemovePackage(t *testing.T) {
-	packageType := "brew"
-	packageToRemove := "bfm"
-	packages := []string{"brew 'vim'", "brew 'bfm'"}
+var _ = Describe("Remove", func() {
+	Describe("When the command is called without any flags", func() {
+		It("Should return an error with info about required flags for specifying package types", func() {
 
-	actual := removePackage(packageType, packageToRemove, packages)
-	expected := []string{"brew 'vim'"}
+		})
+	})
 
-	if !reflect.DeepEqual(actual, expected) {
-		t.Fatalf("Expected %s but got %s", expected, actual)
-	}
-}
+	Describe("When the command is called with a flag specifying the package type and a package name", func() {
+		It("Should return an error with an explanation if the package is not in the Brewfile", func() {
+
+		})
+
+		It("Should not modify the Brewfile if the --dry-run flag is set", func() {
+
+		})
+
+		It("Should remove a tap entry from the Brewfile", func() {
+
+		})
+
+		It("Should remove a cask entry from the Brewfile", func() {
+
+		})
+
+		It("Should remove a mas entry from the Brewfile", func() {
+
+		})
+	})
+
+	Describe("When the command is called for a brew entry without --required or --all", func() {
+		It("Should remove a the brew entry from the Brewfile", func() {
+
+		})
+	})
+
+	Describe("When the command is called for a brew entry --required", func() {
+		It("Should remove a the brew entry and its required dependencies from the Brewfile", func() {
+
+		})
+
+		It("Should not remove required dependencies that are still required by other packages from the Brewfile", func() {
+
+		})
+	})
+
+	Describe("When the command is called for a brew entry --all", func() {
+		It("Should remove a the brew entry and its required, recommended and build dependencies from the Brewfile", func() {
+
+		})
+
+		It("Should not remove any dependencies that are still required by other packages from the Brewfile", func() {
+
+		})
+	})
+})
