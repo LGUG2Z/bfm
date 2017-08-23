3c91defe360be83c657c32d9633a78115c85e9bb
jiqb <gthbji@ml1.net>
2017-08-23 13:19:53 +0100

    Add captureStdout helper for tests in brew package

--- brew/brew_suite_test.go
+++ brew/brew_suite_test.go
@@ -4,10 +4,28 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
+	"bytes"
+	"io"
+	"os"
 	"testing"
 )
 
 func TestBrew(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Brew Suite")
+}
+
+func captureStdout(f func()) string {
+	old := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	f()
+
+	w.Close()
+	os.Stdout = old
+
+	var buf bytes.Buffer
+	io.Copy(&buf, r)
+	return buf.String()
 }
